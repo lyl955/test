fa5864e9722db0f054587573e2625227e60e7177
yonglong.li <yonglong.li@weimob.com>
2019-01-17 14:22:44 +0800

    增加遍历测试

--- src/main/java/com/lyl/test/Main.java
+++ src/main/java/com/lyl/test/Main.java
@@ -1,7 +1,54 @@
 package com.lyl.test;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
+        List<Node> nodes = getNodes();
+
+        for (Node nodet : nodes) {
+            System.out.println(nodet.getId()+":"+nodet.getLevel()+":"+nodet.getPid()+":"+nodet.getSortNum());
+        }
         System.out.println("ok");
+    }
+
+    @NotNull
+    private static List<Node> getNodes() {
+        List<Node> nodes = new ArrayList<Node>();
+        for (int i = 1; i <= 2; i++) {
+            Node node = new Node();
+            node.setId(i);
+            node.setLevel(1);
+            node.setSortNum(i);
+            nodes.add(node);
+        }
+
+        Node node = new Node();
+        node.setId(3);
+        node.setLevel(2);
+        node.setSortNum(1);
+        node.setPid(1);
+        nodes.add(node);
+
+        Node node2 = new Node();
+        node2.setId(4);
+        node2.setLevel(2);
+        node2.setSortNum(1);
+        node2.setPid(2);
+
+        nodes.add(node2);
+
+        Node node3 = new Node();
+        node3.setId(5);
+        node3.setLevel(2);
+        node3.setSortNum(2);
+        node3.setPid(2);
+
+        nodes.add(node3);
+        return nodes;
     }
 }
---
+++ src/main/java/com/lyl/test/Node.java
@@ -0,0 +1,40 @@
+package com.lyl.test;
+
+public class Node {
+    private Integer id;
+    private Integer pid;
+    private Integer level;
+    private Integer sortNum;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Integer getSortNum() {
+        return sortNum;
+    }
+
+    public void setSortNum(Integer sortNum) {
+        this.sortNum = sortNum;
+    }
+}
